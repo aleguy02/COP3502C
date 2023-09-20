0540cc637bd408f3016357533d301671bd11d984
Ale V <alejandro.the.villate@gmail.com>
2023-09-20 14:37:11 -0400

    Updated blackjack.py, wrote pseudocode for the program

--- Projects/Project1/blackjack.py
+++ Projects/Project1/blackjack.py
@@ -1,3 +1,31 @@
 from p1_random import P1Random
 
-rng = P1Random()
+# game elements must be defined including:
+#  number of games played must be defined
+games_played = 0
+#  player's number of wins must be defined
+player_wins = 0
+#  dealer's number of wins must be defined
+dealer_wins = 0
+#  number of ties must be defined
+ties = 0
+
+#  card picking function must be defined
+
+
+#  1. loop for game until game is exited with input: 4
+    #  -1. START GAME (game number) is printed. Each loop increments game number
+
+    #  0. menu is printed, but ONLY after the first loop iteration
+        #  1. player chooses menu option, receives error message if invalid option is input
+
+    #  1. player is dealt their first card from random generation of 1-13
+
+    #  2. card value is printed
+
+    #  3. total hand value is printed
+
+    #  Loop restarts, all values are updated
+    #  The values are: number of games played, the player’s number of wins, the dealer's’ number of wins and the
+# number of ties. Print out all these values as well as the percentage of player wins to the total number games played.
+# Format your percentage value to one decimal point
